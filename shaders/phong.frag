6aefc9e65012b052993c065d69747bd5ce999101
unknown <ro.cailleau@gmail.com>
2020-07-27 18:21:32 +0200

    Made a cube with face normals

--- shaders/phong.frag
+++ shaders/phong.frag
@@ -23,7 +23,7 @@ out vec4 o_color;
 void main()
 {
 	vec3 res = vec3(0.f, 0.f, 0.f);
-
+	vec3 w_normal = normalize(v_w_normal);
 	int i=0;
 	while(i<MAX_LIGHTS)
 	{
@@ -34,7 +34,7 @@ void main()
 			vec3 to_light = light.position - v_w_position;
 			float dist2 = dot(to_light, to_light);
 			to_light = normalize(to_light);
-			float cos_wi = dot(to_light, v_w_normal);
+			float cos_wi = dot(to_light, w_normal);
 			vec3 diff = max(0.f, cos_wi) * u_diffuse * light.Le / dist2;
 			res += diff;
 		}
--- src/OGL.cpp
+++ src/OGL.cpp
@@ -27,7 +27,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 /// Fills the vertices and indices arrays to make a cube, centered on 0, of size 1
 /// </summary>
 template <class Float>
-void makeCube(std::vector<lib::Vertex<Float>>& vertices, std::vector<GLuint>& indices)
+void makeCube(std::vector<lib::Vertex<Float>>& vertices, std::vector<GLuint>& indices, bool use_s_normals=false)
 {
     //        (0,0)                      (h,0)    
     //          x0------------------------x1
@@ -48,40 +48,88 @@ void makeCube(std::vector<lib::Vertex<Float>>& vertices, std::vector<GLuint>& in
     //     |/                        |/    
     //(h,h)x7------------------------x6 (1,1)
 
-    const auto makeVertex = [](Float x, Float y, Float z, Float u, Float v) 
-    {return lib::Vertex<Float>({ x, y, z }, { x, y, z }, { u, v }); };
-
-    // half
     const Float h = Float(0.5);
-    vertices = {
-        makeVertex(-h, h, -h, 0, 0), // x0
-        makeVertex(h, h, -h, h, 0), // x1
-        makeVertex(h, h, h, h, h), // x2
-        makeVertex(-h, h, h, 0, h), // x3
-        makeVertex(-h, -h, -h, h, 0), // x4
-        makeVertex(h, -h, -h, 1, h), // x5
-        makeVertex(h, -h, h, 1, 1), // x6
-        makeVertex(-h, -h, h, h, h), // x7
-    };
+    const Float t = Float(1) / Float(3);
+    const Float tt = Float(2) / Float(3);
 
-    indices.resize(0);
-    const auto addFace = [&indices](GLuint v0, GLuint v1, GLuint v2, GLuint v3)
+    
+
+    if (use_s_normals)
     {
-        indices.push_back(v0);
-        indices.push_back(v1);
-        indices.push_back(v2);
+        const auto makeVertex = [](Float x, Float y, Float z, Float u, Float v)
+        {return lib::Vertex<Float>({ x, y, z }, { x, y, z }, { u, v }); };
 
-        indices.push_back(v2);
-        indices.push_back(v3);
-        indices.push_back(v0);
-    };
+        // half
+        vertices = {
+            makeVertex(-h, h, -h, 0, 0), // x0
+            makeVertex(h, h, -h, h, 0), // x1
+            makeVertex(h, h, h, h, h), // x2
+            makeVertex(-h, h, h, 0, h), // x3
+            makeVertex(-h, -h, -h, h, 0), // x4
+            makeVertex(h, -h, -h, 1, h), // x5
+            makeVertex(h, -h, h, 1, 1), // x6
+            makeVertex(-h, -h, h, h, h), // x7
+        };
 
-    addFace(0, 3, 2, 1); // up (+y)
-    addFace(0, 1, 5, 4); // back (-z)
-    addFace(0, 4, 7, 3); // left (-x)
-    addFace(6, 7, 4, 5); // bottom (-y)
-    addFace(6, 2, 3, 7); // front (+z)
-    addFace(6, 5, 1, 2); // right (+x)
+        indices.resize(0);
+        const auto addFace = [&indices](GLuint v0, GLuint v1, GLuint v2, GLuint v3)
+        {
+            indices.push_back(v0);
+            indices.push_back(v1);
+            indices.push_back(v2);
+
+            indices.push_back(v2);
+            indices.push_back(v3);
+            indices.push_back(v0);
+        };
+
+        addFace(0, 3, 2, 1); // up (+y)
+        addFace(0, 1, 5, 4); // back (-z)
+        addFace(0, 4, 7, 3); // left (-x)
+        addFace(6, 7, 4, 5); // bottom (-y)
+        addFace(6, 2, 3, 7); // front (+z)
+        addFace(6, 5, 1, 2); // right (+x)
+    }
+    else
+    {
+        lib::Vector3<Float> X(1, 0, 0), Y(0, 1, 0), Z(0, 0, 1);
+        const auto sign = [](Float f) {return f > 0 ? Float(1) : (f < 0 ? Float(-1) : Float(0)); };
+        const auto addVertexes = [&](Float x, Float y, Float z, Float u, Float v)
+        {
+            vertices.emplace_back(lib::Vector3<Float>{x, y, z}, sign(x) * X, lib::Vector2<Float>{u, v});
+            vertices.emplace_back(lib::Vector3<Float>{x, y, z}, sign(y) * Y, lib::Vector2<Float>{u, v});
+            vertices.emplace_back(lib::Vector3<Float>{x, y, z}, sign(z) * Z, lib::Vector2<Float>{u, v});
+        };
+        vertices.clear();
+
+        addVertexes(-h, h, -h, 0, 0); // x0
+        addVertexes(h, h, -h, h, 0); // x1
+        addVertexes(h, h, h, h, h); // x2
+        addVertexes(-h, h, h, 0, h); // x3
+        addVertexes(-h, -h, -h, h, 0); // x4
+        addVertexes(h, -h, -h, 1, h); // x5
+        addVertexes(h, -h, h, 1, 1); // x6
+        addVertexes(-h, -h, h, h, h); // x7
+
+        const auto id = [](GLuint xi, GLuint axis) {return xi * 3 + axis; };
+        
+        const auto addFace = [&](GLuint v0, GLuint v1, GLuint v2, GLuint v3, GLuint axis)
+        {
+            indices.push_back(id(v0, axis));
+            indices.push_back(id(v1, axis));
+            indices.push_back(id(v2, axis));
+
+            indices.push_back(id(v2, axis));
+            indices.push_back(id(v3, axis));
+            indices.push_back(id(v0, axis));
+        };
+        addFace(0, 3, 2, 1, 1); // up (+y)
+        addFace(0, 1, 5, 4, 2); // back (-z)
+        addFace(0, 4, 7, 3, 0); // left (-x)
+        addFace(6, 7, 4, 5, 1); // bottom (-y)
+        addFace(6, 2, 3, 7, 2); // front (+z)
+        addFace(6, 5, 1, 2, 0); // right (+x)
+    }
 }
 
 void processInput(GLFWwindow* window, glm::vec3 & moving, float & fov)
@@ -153,7 +201,7 @@ int main()
 
     std::vector<Vertex> vertices;
     std::vector<unsigned int> indices;
-    makeCube(vertices, indices);
+    makeCube(vertices, indices, false);
 
     Mesh cube;
 
